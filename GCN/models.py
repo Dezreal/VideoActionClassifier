d136617616bc197d59c45820e499bc1f7d4563a6
Nya-chu <1311787255@qq.com>
2020-04-09 22:12:11 +0800

    softmax

--- GCN/models.py
+++ GCN/models.py
@@ -12,10 +12,10 @@ class GCN(nn.Module):
         self.dropout = dropout
 
         self.fc = nn.Sequential(
-            nn.Dropout(),
+            nn.Dropout(0.5),
             nn.Linear(25 * noutput * frames_per_video, 100),
             nn.ReLU(),
-            nn.Dropout(),
+            nn.Dropout(0.5),
             nn.Linear(100, 9)
         )
 
@@ -24,6 +24,8 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         # x =  F.log_softmax(x, dim=1)
+        # x = F.avg_pool2d(x.reshape((1, x.shape[0], -1)), 5)
         x = x.flatten()
         x = self.fc(x)
+        x = F.log_softmax(x, dim=0)
         return x
--- GCN/train.py
+++ GCN/train.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn.functional as F
+from torch import optim
 from models import GCN
 from data.utils import build_st_body25_graph
 from data.dataloader import load_features
 
-frames_per_video = 3
+frames_per_video = 5
 model = GCN(3, 16, 100, 0.5, frames_per_video)
-optimizer = torch.optim.Adam(model.parameters(),
-                       lr=0.01, weight_decay=5e-4)
+# optimizer = torch.optim.Adam(model.parameters(),
+#                        lr=0.01, weight_decay=5e-4)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.5)
 
 adj = build_st_body25_graph(frames_per_video)
 train_points, train_labels, test_points, test_labels = load_features("../data/features.txt", frames_per_video)
@@ -23,14 +25,21 @@ test_labels = torch.tensor(test_labels[:, 2], dtype=torch.long).reshape((-1, 1))
 def train(epoch):
     model.train()
     for e in range(0, epoch):
+        outputs = []
         for i in range(0, train_points.shape[0]):
             input = train_points[i]
             label = train_labels[i]
             output = model(input, adj).reshape((1, -1))
-            loss = F.nll_loss(output, label)
-            loss.backward()
-            optimizer.step()
-            print loss
+            outputs.append(output)
+            # loss = F.nll_loss(output, label)
+            # loss.backward()
+            # optimizer.step()
+            # print loss
+        out = torch.cat(outputs, 0)
+        loss = F.nll_loss(out, train_labels.reshape(-1))
+        loss.backward()
+        optimizer.step()
+        print loss
 
 
 def test():
@@ -46,6 +55,6 @@ def test():
 
 
 if __name__ == "__main__":
-    train(1)
+    train(10)
     print("")
     test()
