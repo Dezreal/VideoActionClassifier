f3d1eabdec1b7bd7076280a61c4e4867567ced3c
Nya-chu <1311787255@qq.com>
2020-04-19 19:05:14 +0800

    78%

--- st_gcn/train.py
+++ st_gcn/train.py
@@ -13,7 +13,7 @@ num_classes = 9
 
 # N, in_channels, T_{in}, V_{in}, M_{in}
 
-x, l, t, tl = load_features_for_stgcn("../data/features.txt", 10)
+x, l, tx, tl = load_features_for_stgcn("../data/features.txt", 8)
 labels = torch.tensor(l[:, 2] - 1, dtype=torch.long)
 tl = torch.tensor(tl[:, 2] - 1, dtype=torch.long)
 
@@ -25,15 +25,21 @@ model = Model(in_channel, num_classes, graph, edge_importance_weighting=False)
 optimizer = optim.SGD(model.parameters(), lr=0.09, momentum=0.9)
 
 model.train()
-for i in range(60):
+for i in range(100):
+    # for training
     out = F.log_softmax(model(x), dim=1)
+    # for validation
+    v_out = F.log_softmax(model(tx), dim=1)
     loss = F.nll_loss(out, labels)
     acc = accuracy(out, labels)
-    print("epoch: %d\tloss: %.4f\tacc: %.3f" % (i + 1, loss.item(), acc))
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
+    valid = accuracy(v_out, tl, verbose=False)
+    print("epoch: %d\tloss: %.4f\tacc: %.3f\tval: %.4f" % (i + 1, loss.item(), acc, valid))
+
 # model.eval()
-p = F.log_softmax(model(t), dim=1)
+print("testing")
+p = F.log_softmax(model(tx), dim=1)
 print(accuracy(p, tl, verbose=True))
