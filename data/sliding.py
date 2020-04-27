826a911d6fb9b46d84e73be05a3aaa1b2de66e0b
Nya-chu <1311787255@qq.com>
2020-04-27 13:22:48 +0800

    sliding

--- data/sliding.py
+++ data/sliding.py
@@ -5,49 +5,68 @@ from pyopenpose import op
 from data.feature_creator import OpSingleInstance, cal_rectangle_from_points, cal_area_of_recs
 
 
-# def linspace(start, num, step=1):
-#     result = []
-#     for i in range(0, num):
-#         result.append(start + step * i)
-#     return result
+def get_keypoints(image2process):
+    op_wrapper = OpSingleInstance().op_wrapper
+    datum = op.Datum()
+    datum.cvInputData = image2process
+    op_wrapper.emplaceAndPop([datum])
+    n_people = 0
+    if not datum.poseKeypoints.size == 1:
+        n_people = datum.poseKeypoints.shape[0]
+
+    if n_people == 0:
+        return np.zeros(shape=(25, 3))
+    elif n_people == 1:
+        keypoints = datum.poseKeypoints[0]
+        return keypoints
+    else:
+        recs = cal_rectangle_from_points(datum.poseKeypoints)
+        areas = cal_area_of_recs(recs)
+        main = areas.index(max(areas))
+        keypoints = datum.poseKeypoints[main]
+        return keypoints
 
 
 def sliding(video_path, width, stride=1, dilation=1, padding=(0, 0)):
-    op_wrapper = OpSingleInstance().op_wrapper
+
     frames = []
+    start = 0
+    end = width * dilation
     cap = cv2.VideoCapture(video_path)
+    # for printing msg
     total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    test = [n for n in range(0 - padding[0], total_frame + padding[1])]
+
     for padding_l in range(padding[0]):
-        frames.append(np.zeros(shape=(25, 25, 3)))
+        frames.append(np.zeros(shape=(25, 3)))
     n_first = (width - 1) * dilation + 1
-    for i in range(n_first - padding[0]):
-        datum = op.Datum()
+    for _ in range(n_first - padding[0]):
         boo, frame = cap.read()
-        datum.cvInputData = frame
-        op_wrapper.emplaceAndPop([datum])
-        n_people = 0
-        if not datum.poseKeypoints.size == 1:
-            n_people = datum.poseKeypoints.shape[0]
+        frames.append(get_keypoints(frame))
 
-        if n_people == 0:
-            frames.append(np.zeros(shape=(25, 25, 3)))
-        elif n_people == 1:
-            keypoints = datum.poseKeypoints[0]
-            frames.append(keypoints)
+    n = 0
+    padding_r = padding[1]
+    while True:
+        if n % stride == 0:
+            index = slice(start, end, dilation)
+            print(test[index])
+            yield frames[index]
+            start = start + stride
+            end = end + stride
+        n = n + 1
+
+        boo, frame = cap.read()
+        if not boo:
+            if padding_r <= 0:
+                break
+            else:
+                frames.append(np.zeros(shape=(25, 3)))
+                padding_r = padding_r - 1
         else:
-            recs = cal_rectangle_from_points(datum.poseKeypoints)
-            areas = cal_area_of_recs(recs)
-            main = areas.index(max(areas))
-            keypoints = datum.poseKeypoints[main]
-            frames.append(keypoints)
-
-    index = slice(0, width * dilation, dilation)
-    yield frames[index]
-
-
-
+            frames.append(get_keypoints(frame))
 
 
 if __name__ == "__main__":
-    arr = np.linspace(0, 10, 11)
-    print(arr[slice(0, 6, 1)])
+    path = "/datasets/Florence_3d_actions/GestureRecording_Id1actor1idAction1category1.avi"
+    for i in sliding(path, 4, stride=2, dilation=3, padding=(3, 3)):
+        print(str(i.__len__()) + str(i[0].shape))
