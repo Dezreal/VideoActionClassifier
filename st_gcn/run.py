631082c22d42010d3a7d19e947470241ef396d80
Nya-chu <1311787255@qq.com>
2020-05-06 20:14:14 +0800

    fix `save`

--- st_gcn/run.py
+++ st_gcn/run.py
@@ -1,9 +1,15 @@
 import torch
 from torch.nn import functional as F
 from data.sliding import sliding
+from graph import Graph
+from model import Model
 from utils import get_action_name
 
-model = torch.load("./st_gcn/model.pt")
+graph_args = {"strategy": "spatial"}
+graph = Graph(**graph_args)
+
+model = Model(3, 9, graph, edge_importance_weighting=False)
+model.load_state_dict(torch.load("model_param.pt"))
 model.eval()
 path = "/datasets/Florence_3d_actions/GestureRecording_Id1actor1idAction1category1.avi"
 for i in sliding(0, 8, stride=1, dilation=1, padding=(1, 1)):
